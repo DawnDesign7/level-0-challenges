7d9145a27559e8656e3e9cade8cbe3d45f726fe4
DawnDesign7 <dawnmoraa@gmail.com>
2021-10-21 09:02:22 +0300

    add task 0.5 area of triangle

---
+++ task0.5.js
@@ -0,0 +1,9 @@
+// Area using Semiperimeter method
+function areaTri(side1, side2, side3) {
+    var semiPer = (side1 + side2 + side3) / 2;
+    var area = Math.sqrt(semiPer * ((semiPer - side1) * (semiPer - side2)* (semiPer - side3)));
+
+    return area;
+}
+
+areaTri(3, 4, 5);
