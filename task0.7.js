899337589bef65968d41a6f9c235a89fb16c228c
DawnDesign7 <dawnmoraa@gmail.com>
2021-10-21 15:44:16 +0300

    removed comments in task 0.7 answer

--- task0.7.js
+++ task0.7.js
@@ -1,4 +1,4 @@
-// Celsius to Farenheit (0C * 1.8) + 32 = 32°F
+// Celsius to Farenheit
 function celToFaren(celsius) {
     let farenheit = (celsius * 1.8) + 32;
     return farenheit;
@@ -6,10 +6,9 @@ function celToFaren(celsius) {
 
 celToFaren(30);
 
-// Farenheit to Celsius (32F -32) * 1.8 = 0C 
+// Farenheit to Celsius 
 function farenToCel(farenheit) {
     let celsius = (farenheit -32) * 1.8;
     return celsius;
 }
 
-farenToCel(32);
