fa96e13d6194d951bc9f7affb33394fbc5adca5e
DawnDesign7 <dawnmoraa@gmail.com>
2021-10-25 19:36:48 +0300

    removed prompt in task 0.4

--- task0.4.js
+++ task0.4.js
@@ -1,9 +1,9 @@
-function evenOrOdd(i) {
-    var i = prompt("Insert number to check if it's odd or even")
-    if(i % 2 == 0) {
-        console.log(even);
+function evenOrOdd(num) {
+    if(num % 2 == 0) {
+        console.log("even");
     } else {
-        console.log(odd);
+        console.log("odd");
     }
 
 }
+evenOrOdd(32);
