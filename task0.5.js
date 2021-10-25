8dad3dfa4f7d03d86eb1561972f0f5cc0baba1c1
DawnDesign7 <dawnmoraa@gmail.com>
2021-10-25 19:38:02 +0300

    replace 'var' with 'let' for task 0.5

--- task0.5.js
+++ task0.5.js
@@ -1,7 +1,7 @@
 // Area using Semiperimeter method
 function areaTri(side1, side2, side3) {
-    var semiPer = (side1 + side2 + side3) / 2;
-    var area = Math.sqrt(semiPer * ((semiPer - side1) * (semiPer - side2)* (semiPer - side3)));
+    let semiPer = (side1 + side2 + side3) / 2;
+    let area = Math.sqrt(semiPer * ((semiPer - side1) * (semiPer - side2)* (semiPer - side3)));
 
     return area;
 }
