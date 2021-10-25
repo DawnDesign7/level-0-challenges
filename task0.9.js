ae1888f26f3e7635ea92621824e5e68d58387846
DawnDesign7 <dawnmoraa@gmail.com>
2021-10-25 19:08:12 +0300

    remove prompt task 0.9

--- task0.9.js
+++ task0.9.js
@@ -1,6 +1,7 @@
-function vowelFinder(word) {
-    var word = prompt("enter a random word to isolate it's vowels");
-    const vowels = word.match(/[aeiou]/gi)
+function vowelFinder(string) {
+    const vowels = string.match(/[aeiou]/gi)
     console.log("Vowels: " + vowels);
 
 }
+vowelFinder("umuzi");
+
