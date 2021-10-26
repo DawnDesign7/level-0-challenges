292f96f65f2aba5af7ad79ca8f9362599a166295
DawnDesign7 <dawnmoraa@gmail.com>
2021-10-26 11:46:35 +0300

    redo task 0.9 to avoid repetition ofvowels in output

--- task0.9.js
+++ task0.9.js
@@ -1,7 +1,12 @@
-function vowelFinder(string) {
-    const vowels = string.match(/[aeiou]/gi)
-    console.log("Vowels: " + vowels);
-
+function vowelFinder(word) {
+    let vowels = "";
+    word = word.toLowerCase();
+    for(var i = 0; i < word.length; i++) {
+        if(word.indexOf("a") === i || word.indexOf("e") === i || word.indexOf("i") === i || word.indexOf("o") === i ||word.indexOf("u") === i) {
+            vowels += word[i] + ", ";
+        }
+    }
+    console.log("Vowels: " + vowels)
 }
 vowelFinder("umuzi");
 
